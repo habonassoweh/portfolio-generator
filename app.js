ad4634574124e7f1beb2b75bd0d8b05f24b86f27
Habon Assoweh <habonassoweh@Habons-Air>
2022-01-19 00:23:24 -0500

    HTML template literal

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-//const generatePage = () => 'Name: Jane, Github: janehub';
+const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
-    return `
-      Name: ${userName}
-      GitHub: ${githubName}
-    `;
-  };
-  
-console.log(generatePage('Jane', 'janehub'));
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
 
-
-
-
-
-
-
-
-
-
-
-//const printProfileData = profileDataArr => {
-    // This...
-    //for (let i = 0; i < profileDataArr.length; i += 1) {
-     // console.log(profileDataArr[i]);
-    //}
-  
-    //console.log('================');
-  
-    // Is the same as this...
-   //profileDataArr.forEach((profileItem) => {
-    //  console.log(profileItem)
-   // });
- // };
-
+console.log(name, github);
+console.log(generatePage(name, github));
