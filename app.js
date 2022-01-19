7a076cc63fc1667743034ff58646016ad3778408
Habon Assoweh <habonassoweh@Habons-Air>
2022-01-18 22:14:45 -0500

    created branch

---
+++ app.js
@@ -0,0 +1,38 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+//const generatePage = () => 'Name: Jane, Github: janehub';
+
+const generatePage = (userName, githubName) => {
+    return `
+      Name: ${userName}
+      GitHub: ${githubName}
+    `;
+  };
+  
+console.log(generatePage('Jane', 'janehub'));
+
+
+
+
+
+
+
+
+
+
+
+
+//const printProfileData = profileDataArr => {
+    // This...
+    //for (let i = 0; i < profileDataArr.length; i += 1) {
+     // console.log(profileDataArr[i]);
+    //}
+  
+    //console.log('================');
+  
+    // Is the same as this...
+   //profileDataArr.forEach((profileItem) => {
+    //  console.log(profileItem)
+   // });
+ // };
+
